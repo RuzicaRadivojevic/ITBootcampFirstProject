98984a7b31133e3423f018a69697b8c0c7ba4b32
RuzicaRadivojevic <ruzica.radivojevic995@gmail.com>
2022-09-03 23:08:43 +0200

    Refactoring

--- src/RuzicaRadivojevic.java
+++ src/RuzicaRadivojevic.java
@@ -6,11 +6,11 @@ public class RuzicaRadivojevic {
 
     public static int[] nizOfInt() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Unesite duzinu niza: ");
+        //System.out.println("Unesite duzinu niza: ");
         int n = sc.nextInt();
         int[] niz1 = new int[n];
         for (int i = 0; i < niz1.length; i++) {
-            System.out.println("Unesite " + i + ". clan niza: ");
+            System.out.print("Unesite " + i + ". clan niza: ");
             niz1[i] = sc.nextInt();
         }
         System.out.println("Uneti niz je: " + Arrays.toString(niz1));
@@ -43,7 +43,24 @@ public class RuzicaRadivojevic {
         return niz1;
     }
 
-    //Implementirati metodu int zbirCifara(int n) koja sabira cifre sve dok rezultat ne bude samo jedna cifra.
+    //1. Implementirati metodu int brojPonavljanja(String s, String recenica) koja vraća broj ponavljanja stringa "s" u rečenici. Hint: istražiti s.split("") metodu.
+    //       Npr. ("cao", "cao kako si cao") -> 2
+
+    public static int[] brojPonavljanja(String s, String recenica) {
+        String[] razbijenaRec = recenica.split(" ");
+
+        int brojac = 0;
+
+        for (int i = 0; i < razbijenaRec.length; i++) {
+            if (s.equals(razbijenaRec[i])) {
+                brojac++;
+            }
+        }
+
+        return new int[]{brojac};
+
+    }
+    //2. Implementirati metodu int zbirCifara(int n) koja sabira cifre sve dok rezultat ne bude samo jedna cifra.
     //        Npr. 1234 -> 1, 43 -> 7, 55 -> 1, 556 -> 7
 
     public static int zbirCifara(int korisnikovBroj) {
@@ -75,7 +92,7 @@ public class RuzicaRadivojevic {
     }
 
 
-//    Implementirati metodu boolean stepenBroja(int n, int m) koja vraća true ukoliko je n stepen broja m.
+//   3. Implementirati metodu boolean stepenBroja(int n, int m) koja vraća true ukoliko je n stepen broja m.
 //    Npr: (9, 3) -> true, (125, 5) -> true, (13, 4) -> false
 
     public static boolean stepenBroja(int n, int m) {
@@ -92,7 +109,7 @@ public class RuzicaRadivojevic {
     }
 
 
-//    Implementirati metodu int[] zbirIndexaNizaMeta(int[] niz, int meta) koja vraća indekse dva elementa koji kada se saberu daju rezultat koji je jednak meti.
+//   4. Implementirati metodu int[] zbirIndexaNizaMeta(int[] niz, int meta) koja vraća indekse dva elementa koji kada se saberu daju rezultat koji je jednak meti.
 //        Npr. ({2, 3, 10, -2}, 5) -> {0, 1}
 
     public static int[] zbirIndexaNizaMeta(int[] niz, int meta) {
@@ -121,7 +138,7 @@ public class RuzicaRadivojevic {
 
     }
 
-//     Implementirati metodu String toBinary(int n) koja vraća broj n u binarnom formatu. Uzeti apsolutnu vredost n.
+//    5. Implementirati metodu String toBinary(int n) koja vraća broj n u binarnom formatu. Uzeti apsolutnu vredost n.
 //             Npr. 10 -> 1010, 100 -> 1100100
 
     public static String toBinary(int n) {
@@ -141,24 +158,8 @@ public class RuzicaRadivojevic {
         }
         return rezultat;
     }
-    // Implementirati metodu int brojPonavljanja(String s, String recenica) koja vraća broj ponavljanja stringa "s" u rečenici. Hint: istražiti s.split("") metodu.
-    //       Npr. ("cao", "cao kako si cao") -> 2
 
-    public static int[] brojPonavljanja(String s, String recenica) {
-        String[] razbijenaRec = recenica.split(" ");
-        int brojac = 0;
-
-        for (int i = 0; i < razbijenaRec.length; i++) {
-            if (s.equals(razbijenaRec[i])) {
-                brojac++;
-            }
-        }
-
-        return new int[]{brojac};
-
-    }
-
-    //Implementirati metodu int[] pomeriNule(int[] niz) koja vraća niz u kome su sve nule pomerene na kraj niza.
+    //6. Implementirati metodu int[] pomeriNule(int[] niz) koja vraća niz u kome su sve nule pomerene na kraj niza.
     //      Npr. {1, 0, 0, 2, 3} -> {1, 2, 3, 0, 0}
 
     public static int[] pomeriNule(int[] niz) {
@@ -177,7 +178,7 @@ public class RuzicaRadivojevic {
     }
 
 
-    // Implementirati metodu int[] izbaciDuplikate(int[] niz) koja vraća niz bez duplikata.
+    //7. Implementirati metodu int[] izbaciDuplikate(int[] niz) koja vraća niz bez duplikata.
     //       Npr. {1, 2, 3, 3} -> {1, 2, 3}, {3, 4, 3, 4, 3, 4, 5} -> {3, 4, 5}
 
     public static int[] izbaciDuplikate(int[] niz) {
@@ -201,13 +202,46 @@ public class RuzicaRadivojevic {
 
     public static void main(String[] args) {
 
-        //System.out.println("1.Zadatak - Ukupan jednocifren zbir svih unetih cifara je broj " + zbirCifara(12345) + ".");
-        //System.out.println("\n2. Zadatak - Da li je uneti broj stepen broja: " + stepenBroja(5, 125));
-        //System.out.println("\n3. Zadatak - Niz indeksa elemenata koji daju izabrani broj je :" + Arrays.toString(zbirIndexaNizaMeta(nizOfInt(), 5)));
-        //System.out.println("\n4. Zadatak - Uneti broj prikazan u binarnom formatu je : " + (toBinary(50)));
-        //System.out.println("\n5. Zadatak - Trazena rec se ponavlja " + Arrays.toString(brojPonavljanja("cao", "cao kako si cao  majko danas")) + " puta u recenici.");
-        //  System.out.println("\n6. Zadatak - Niz sa pomerenim nulama na kraju je: " + Arrays.toString(pomeriNule(nizOfInt())));
-        System.out.println("\n7. Zadatak - Niz bez duplikata je: " + Arrays.toString(izbaciDuplikate(nizOfInt())));
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("############################################################################################");
+        System.out.print("\n1. Zadatak - Unesite izabranu rec koju zelite da proverite koliko se puta ponavlja u recenici: ");
+        String rec = sc.nextLine();
+        System.out.print("\nUnesite recenicu u kojoj zelite da proverite koliko se prethodno uneta rec ponavlja: ");
+        String recenica = sc.nextLine();
+        System.out.println("\nTrazena rec se ponavlja " + Arrays.toString(brojPonavljanja(rec, recenica)) + " puta u recenici.");
+
+        System.out.println("\n############################################################################################");
+        System.out.print("\n2.Zadatak - Unesite dvocifren, trocifren ili veci broj za koji zelite da izracunate jednocifren zbir: ");
+        int n = sc.nextInt();
+        System.out.println("\nUkupan jednocifren zbir svih unetih cifara je broj " + zbirCifara(n) + ".");
+
+        System.out.println("\n############################################################################################");
+        System.out.print("\n3. Zadatak - Unesite broj za proveru da li je stepen drugog unetog broja: ");
+        int broj = sc.nextInt();
+        System.out.print("\nUnesite broj za koji zelite da proverite da li je stepen prethodno unetog broja:");
+        int stepen = sc.nextInt();
+        System.out.println("\nDa li je uneti broj stepen broja: " + stepenBroja(broj, stepen));
+
+        System.out.println("\n############################################################################################");
+        System.out.print("\n4. Zadatak - Unesi broj koji zelis da dobijes od dva elementa niza: ");
+        int meta = sc.nextInt();
+        System.out.print("\nUnesite duzinu niza za indexe elemenata koji daju izabrani broj: ");
+        System.out.println("\nNiz indeksa elemenata koji daju izabrani broj je :" + Arrays.toString(zbirIndexaNizaMeta(nizOfInt(), meta)));
+
+        System.out.println("\n############################################################################################");
+        System.out.print("\n5. Zadatak - Unesite broj koji zelite da prikazete u binarnom formatu: ");
+        int binar = sc.nextInt();
+        System.out.println("\nUneti broj prikazan u binarnom formatu je : " + (toBinary(binar)));
+
+        System.out.println("\n############################################################################################");
+        System.out.print("\n6. Zadatak - Unesite duzinu niza gde pomeramo nule na kraj niza: ");
+        System.out.println("\n Niz sa pomerenim nulama na kraju je: " + Arrays.toString(pomeriNule(nizOfInt())));
+
+        System.out.println("\n############################################################################################");
+        System.out.print("\n7. Zadatak - Unesite duzinu niza gde izbacujemo duplikate: ");
+        System.out.println("\nNiz bez duplikata je: " + Arrays.toString(izbaciDuplikate(nizOfInt())));
+        System.out.println("\n############################################################################################");
 
     }
 }
